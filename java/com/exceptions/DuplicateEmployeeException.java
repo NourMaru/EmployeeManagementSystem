2018844b3537b4c52bcf14de4875cc4d48d67dc2
NourMaru <32373167+NourMaru@users.noreply.github.com>
2019-09-25 15:36:02 -0400

    Sep25 Updated code

---
+++ java/com/exceptions/DuplicateEmployeeException.java
@@ -0,0 +1,17 @@
+package com.exceptions;
+
+/**
+ * Duplicate HourlyEmployee Exception Class
+ */
+public class DuplicateEmployeeException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+
+    //@param message the message of the exception thrown
+
+    public DuplicateEmployeeException(String message) {
+        super(message);
+    }
+
+}
---
+++ java/com/exceptions/InvalidUserDataException.java
@@ -0,0 +1,17 @@
+package com.exceptions;
+
+public class InvalidUserDataException extends Exception {
+
+    //code used to remove the Java generate warning
+    private static final long serialVersionUID = 1L;
+
+    //@param message the message of the exception thrown
+    public InvalidUserDataException(String message) {
+        super(message);
+    }
+
+    //Invalid Id Exception
+    public InvalidUserDataException() {
+        super();
+    }
+}
